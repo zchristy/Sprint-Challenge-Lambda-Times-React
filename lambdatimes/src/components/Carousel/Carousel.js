4d45e4d5741f516736a94876969da0a80168c7ed
zchristy <zchristy44@gmail.com>
2019-05-03 11:59:54 -0500

    Completed Carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,33 +1,53 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
-// Complete this Carousel 
+// Complete this Carousel
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      images: [],
+      count: 0
     }
   }
   componentDidMount(){
-
+    this.setState({
+      images: carouselData
+    });
   }
 
-  leftClick = () => {
-
+  leftClick = (event) => {
+    if(this.state.count === 0) {
+      this.setState({
+        count: this.state.images.length - 1
+      })
+    } else {
+      this.setState({
+        count: this.state.count - 1
+      })
+    }
   }
 
-  rightClick = () => {
-
+  rightClick = (event) => {
+    if(this.state.count === 3) {
+      this.setState({
+        count: 0
+      })
+    } else {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.images[this.state.count]} style={{display: 'block'}} />
   }
-  
+
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -75,6 +76,7 @@ export default class Content extends Component {
           selectTabHandler={this.changeSelected}
           tabs={this.state.tabs}
         />
+        <Carousel />
         <Cards cards={this.filterCards()} />
       </div>
     );
